67a3020d072742bceec6600f9743e2f1ece35e92
Dima <dima.tsymbal@outlook.com>
2023-08-09 12:53:20 +0300

    fix home

--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,22 @@ import { Card, CardContent, Container, Grid } from "@mui/material";
 import { useState } from "react";
 import Button from "@/components/Button";
 export const getStaticProps = async () => {
+  if (!process.env.API_HOST) {
+    console.error("API_HOST is not defined");
+    return {
+      props: {
+        gallery: null,
+      },
+    };
+  }
+
   try {
     const res = await fetch(`${process.env.API_HOST}/gallery`);
     const data = await res.json();
 
-    if (!data) {
-      return {
-        notFound: true,
-      };
-    }
-
     return {
       props: {
-        gallery: data,
+        gallery: data || null,
       },
     };
   } catch (error) {
@@ -50,19 +53,23 @@ const Home = ({ gallery }) => {
 
           <div style={{ marginTop: "20px" }}>
             <Grid container spacing={2}>
-              {gallery.slice(0, part).map(({ id, title, url }) => (
-                <Grid item xs={12} sm={6} md={4} key={id}>
-                  <Card className={styles.card}>
-                    <Image
-                      src={url}
-                      alt="galleryImage"
-                      width={500}
-                      height={400}
-                    />
-                    <p>{title}</p>
-                  </Card>
-                </Grid>
-              ))}
+              {gallery && Array.isArray(gallery) ? (
+                gallery.slice(0, part).map(({ id, title, url }) => (
+                  <Grid item xs={12} sm={6} md={4} key={id}>
+                    <Card className={styles.card}>
+                      <Image
+                        src={url}
+                        alt="galleryImage"
+                        width={500}
+                        height={400}
+                      />
+                      <p>{title}</p>
+                    </Card>
+                  </Grid>
+                ))
+              ) : (
+                <p>No gallery data available</p>
+              )}
             </Grid>
           </div>
 
